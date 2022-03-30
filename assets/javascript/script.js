e0a84fb6c9de4af96ca4e4ace6df9b6d7ed21635
llattanzi <lauren71442@gmail.com>
2022-03-30 12:32:41 -0400

    add searched artist to local storage. dynamically generate artist history in search dropdown options

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,7 +4,7 @@ var artistHistory = [];
 // # of songs to display for each artist searched
 var songLimit = 8;
 // max length of artists to store
-var historyLimit = 10; 
+var historyLimit = 8; 
 
 var getSongs = function(artist) {
     // must add quotes to artist name for url to accept artists with spaces in name 
@@ -38,6 +38,9 @@ var getSongs = function(artist) {
 }
 
 var displaySongs = function(data, artist) {
+    
+    addHistory(artist);
+
     // display artist name on page
     $(".band-name-searched").text(artist);
     for (i = 0; i < songLimit; i++) {
@@ -68,18 +71,50 @@ var getVideos = function(searchTerm) {
     var apiKey = "AIzaSyBLlzSDytVY85LZFVhQJHeHMXn-jQsel7M";
     var apiUrl2 = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + searchTerm + "&key=" + apiKey;
     fetch(apiUrl2)
-    .then(function(response) {
-        return response.json();
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            var videoID = data.items[0].id.videoId;
+            console.log(data)
+            var videoLink = "https://www.youtube.com/watch?v=" + data.items[0].id.videoId;
+            console.log(videoLink);
+            $("#video").attr("src", "https://www.youtube.com/embed/" + videoID);
+            $("#content-link").show();
     })
-    .then(function(data) {
-        var videoID = data.items[0].id.videoId;
-        console.log(data)
-        var videoLink = "https://www.youtube.com/watch?v=" + data.items[0].id.videoId;
-        console.log(videoLink);
-        $("#video").attr("src", "https://www.youtube.com/embed/" + videoID);
-        $("#content-link").show();
-})
 
+}
+
+// add artist to search dropdown and store to local storage
+var addHistory = function(artist) {
+    // create new option in search dropdown
+    var optionEl = $("<option>")
+    .attr("value", artist);
+
+    // if history reaches the limit, remove oldest search result element
+    if ($("#search-history").length == historyLimit) {
+        // remove from page
+        $("#search-history").eq(0).remove();
+        // remove from local storage variable
+        artistHistory = artistHistory.slice(1, artistHistory.length);
+    }
+
+    // add new artist to artist history list on page
+    $("#search-history").append(optionEl);
+    // add new artist to local storage
+    artistHistory.push(artist);
+    localStorage.setItem("artists", JSON.stringify(artistHistory));
+}
+
+// load search history from local storage and add to search dropdown menu
+var loadHistory = function () {
+    for (i = 0; i < artistHistory.length; i++) {
+        // create new option in search dropdown
+        var optionEl = $("<option>")
+            .attr("value", artistHistory[i]);
+            // append to end of options list
+            $("#search-history").append(optionEl);
+    }
 }
 
 $("#submit-btn").click(function() {
@@ -105,3 +140,15 @@ $("#close-modal").click(function() {
     $(".modal").hide();
     console.log("modal close")
 })
+
+// when user clicks option in search history dropdown, retrieve and print artist data
+$("#search-history").on("click", "option", function() {
+    artist = $(this).attr("value");
+    getSongs(artist);
+})
+
+// if there are saved artists in local storage, load to page
+if (localStorage.getItem("artists")) {
+    artistHistory = JSON.parse(localStorage.getItem("artists"));
+    loadHistory();
+}
